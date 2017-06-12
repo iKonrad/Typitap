0de0a28bd545d29518e68539d539f6b217f16fbc
Konrad Jarosinski <konrad@jarosinski.uk>
2017-06-13 00:36:14 +0100

    Testing ctime for ubuntu

--- server/services/utils/file-utils.go
+++ server/services/utils/file-utils.go
@@ -4,16 +4,25 @@ import (
 	"time"
 	"os"
 	"syscall"
+	"github.com/iKonrad/typitap/server/config"
 )
 
 func StatTimes(name string) (atime, mtime, ctime time.Time, err error) {
+
 	fi, err := os.Stat(name)
 	if err != nil {
 		return
 	}
 	mtime = fi.ModTime()
 	stat := fi.Sys().(*syscall.Stat_t)
-	atime = time.Unix(int64(stat.Atimespec.Sec), int64(stat.Atimespec.Nsec))
-	ctime = time.Unix(int64(stat.Ctimespec.Sec), int64(stat.Ctimespec.Nsec))
+
+		//atime = time.Unix(int64(stat.Atimespec.Sec), int64(stat.Atimespec.Nsec))
+		//ctime = time.Unix(int64(stat.Ctimespec.Sec), int64(stat.Ctimespec.Nsec))
+
+		atime = time.Unix(int64(stat.Atim.Sec), int64(stat.Atim.Nsec))
+		ctime = time.Unix(int64(stat.Ctim.Sec), int64(stat.Ctim.Nsec))
+
+
+
 	return
 }
